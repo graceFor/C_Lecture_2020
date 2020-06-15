8e121dfb757f3ae05b4f19be39ae74b0aace4c4e
hyunkyung <tbgusrud12@gmail.com>
2020-06-15 21:30:52 +0900

    1. Lecture 1_emoticon

---
+++ Lecture 1/emoticon.c
@@ -0,0 +1,15 @@
+/*This program prints "A shy pitch emoticon" on the screen.*/
+
+#include <stdio.h>
+
+int main() {
+	printf("          /\\\n");
+	printf("        /    \\\n");
+	printf("     @         @\n");
+	printf("   @             @\n");
+	printf(" @  ///      ///   @\n");
+	printf(" @        @        @\n");
+	printf("   @   '      ' @\n");
+
+	return 0;
+}
